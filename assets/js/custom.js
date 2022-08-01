d94a56ff5106089b24b25626e8514b961e76553a
Ravi Pansuriya <49712335+imravipansuriya@users.noreply.github.com>
2022-08-02 00:09:07 +0530

    Delete assets directory

--- assets/js/custom.js
+++
@@ -1,5 +0,0 @@
-var currentMonth = document.getElementById('currant_month');
-const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-var date = new Date();
-var getMonth = month[date.getMonth()];
-var setMonth = currentMonth.innerText = 'Plan for the month of ' + getMonth;
